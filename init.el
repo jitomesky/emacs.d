093163fbed3e0f92c5ad19c38168cc2d53ed4d7a
Toshifumi NISHINAGA <dev@jit0.me>
2015-06-26 14:06:57 +0900

    - add region-bindings-mode - set keybind for multiple-cursors

--- init.el
+++ init.el
@@ -7,3 +7,16 @@
 (package-initialize)
 
 (require 'multiple-cursors)
+
+(require 'region-bindings-mode)
+(region-bindings-mode-enable)
+
+(define-key region-bindings-mode-map "a" 'mc/mark-all-like-this)
+(define-key region-bindings-mode-map "p" 'mc/mark-previous-like-this)
+(define-key region-bindings-mode-map "n" 'mc/mark-next-like-this)
+(define-key region-bindings-mode-map "u" 'mc/unmark-next-like-this)
+(define-key region-bindings-mode-map "U" 'mc/unmark-previous-like-this)
+(define-key region-bindings-mode-map "s" 'mc/skip-to-next-like-this)
+(define-key region-bindings-mode-map "S" 'mc/skip-to-previous-like-this)
+(define-key region-bindings-mode-map ";" 'multiple-cursors-mode)
+
